ae3ce70e734e3602a8433cc70cbfe3251a8bae6b
czhang <czhang79@ccs.neu.edu>
2017-03-28 00:19:42 -0400

    selectedVideo

--- src/index.js
+++ src/index.js
@@ -3,24 +3,35 @@ import ReactDom from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 const API_KEY = 'AIzaSyBiMhTo2uovsnXq4fRilS9zYT-Js-CKWYc';
 
 class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { videos: [] };
+    this.state = { 
+      videos: [],
+      selectedVideo: null
+    };
 
     YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-      this.setState({ videos });
+      this.setState({ 
+        videos: videos,
+        selectedVideo: videos[0]
+        });
       });
   }
 
   render() {
+
     return (
       <div>
         <SearchBar />
-        <VideoList />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({selectedVideo}) } 
+          videos = {this.state.videos} />
       </div>
     );
   }
