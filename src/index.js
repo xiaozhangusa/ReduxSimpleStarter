fe69694c4eb2d805f9c60cf2e90aa97e07da1dc2
czhang <czhang79@ccs.neu.edu>
2017-03-27 21:54:21 -0400

    1 chapter

--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import React, { Component } from 'react';
+import ReactDom from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+const API_KEY = 'AIzaSyBiMhTo2uovsnXq4fRilS9zYT-Js-CKWYc';
 
-import App from './components/app';
-import reducers from './reducers';
+class App extends Component {
+  constructor(props) {
+    super(props);
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+    this.state = { videos: [] };
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList />
+      </div>
+    );
+  }
+}
+
+// Take this component's generated HTML and put it on the page (in the DOM)
+ReactDom.render(<App />, document.querySelector('.container'));
